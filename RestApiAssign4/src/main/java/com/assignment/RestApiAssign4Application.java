b31ab5a301d684c86f22771ab6a474fa4068fc8b
Varikol05 <115691392+Varikol05@users.noreply.github.com>
2023-01-06 09:59:42 +0530

    This is Rest API assignment

---
+++ RestApiAssign4/src/main/java/com/assignment/RestApiAssign4Application.java
@@ -0,0 +1,13 @@
+package com.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiAssign4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiAssign4Application.class, args);
+	}
+
+}
---
+++ RestApiAssign4/src/test/java/com/assignment/RestApiAssign4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiAssign4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
