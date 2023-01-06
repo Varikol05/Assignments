78a0e83c6e6043130dde7046393f36d48b192f4f
Varikol05 <115691392+Varikol05@users.noreply.github.com>
2023-01-06 09:46:51 +0530

    This is Spring Core Assignment

---
+++ Spring8910/src/main/java/com/assignments/App.java
@@ -0,0 +1,20 @@
+package com.assignments;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+
+
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        @SuppressWarnings("resource")
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("HelloWorld.xml");
+        HelloWorld hw = context.getBean(HelloWorld.class);
+        System.out.println(hw.getMessage());
+        context.registerShutdownHook();
+    }
+}
