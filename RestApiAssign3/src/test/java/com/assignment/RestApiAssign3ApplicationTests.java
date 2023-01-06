11d3fbbb77994044294a878471409eb2c0cbcdaf
Varikol05 <115691392+Varikol05@users.noreply.github.com>
2023-01-06 09:58:04 +0530

    This is Rest API Assignment

---
+++ RestApiAssign3/src/test/java/com/assignment/RestApiAssign3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiAssign3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
